70cad71140fbc6972c6ff32fd4102344c8f432e3
LinXi <LinXi@qq.com>
2024-01-24 22:48:28 +0800

    second commit

--- src/main/java/com/xxl/git/GitTest.java
+++ src/main/java/com/xxl/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xxl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git-test");
+        System.out.println("git-test2");
     }
 }
